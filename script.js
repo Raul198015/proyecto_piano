531cdced5803f47980f653b3ab448a78eddf36c7
raulenzo <raru69@hotmail.com>
2023-06-10 02:48:53 +0200

    cambios en html y script

--- script.js
+++ script.js
@@ -1,18 +1,237 @@
-const container = document.getElementById('container');
-const keysToDivs = {
-  'KeyA': 'Div A',
-  'KeyB': 'Div B',
-  'KeyC': 'Div C',
-  // Agrega más teclas y contenido de div aquí
-};
+let div1 = document.querySelector('#C1');
+let div2 = document.querySelector('#C1sharp');
+let div3 = document.querySelector('#D1');
+let div4 = document.querySelector('#D1sharp');
+let div5 = document.querySelector('#E1');
+let div6 = document.querySelector('#F1');
+let div7 = document.querySelector('#F1sharp');
+let div8 = document.querySelector('#G1');
+let div9 = document.querySelector('#G1sharp');
+let div10 = document.querySelector('#A1');
+let div11 = document.querySelector('#A1sharp');
+let div12 = document.querySelector('#B1');
+let div13 = document.querySelector('#C2');
+let div14 = document.querySelector('#C2sharp');
+let div15 = document.querySelector('#D2');
+let div16 = document.querySelector('#D2sharp');
+let div17 = document.querySelector('#E2');
+let div18 = document.querySelector('#F2');
+let div19 = document.querySelector('#F2sharp');
+let div20 = document.querySelector('#G2');
+let div21 = document.querySelector('#G2sharp');
+let div22 = document.querySelector('#A3');
+let div23 = document.querySelector('#A3sharp');
+let div24 = document.querySelector('#B3');
 
-document.addEventListener('keydown', function(event) {
-  const key = event.code;
+
+
+document.addEventListener('keydown',  function(evento) {
+  if (evento.code === 'KeyZ') {
+    div1.style.backgroundColor = 'yellow';
+    } 
+    
+    else if (evento.code === 'KeyX') {
+    div2.style.backgroundColor = 'red';
+    }
+  if (evento.code === 'KeyC') {
+    div3.style.backgroundColor = 'yellow';
+    } else if (evento.code === 'KeyV') {
+    div4.style.backgroundColor = 'red';
+    }
+  if (evento.code === 'KeyB') {
+    div5.style.backgroundColor = 'yellow';
+    } else if (evento.code === 'KeyN') {
+    div6.style.backgroundColor = 'yellow';
+    }
+  if (evento.code === 'KeyM') {
+    div7.style.backgroundColor = 'red';
+    } else if (evento.code === 'KeyA') {
+    div8.style.backgroundColor = 'yellow';
+    }
+  if (evento.code === 'KeyS') {
+    div9.style.backgroundColor = 'red';
+    } else if (evento.code === 'KeyD') {
+    div10.style.backgroundColor = 'yellow';
+    }
+  if (evento.code === 'KeyF') {
+    div11.style.backgroundColor = 'red';
+    } else if (evento.code === 'KeyG') {
+    div12.style.backgroundColor = 'yellow';
+    }
+  if (evento.code === 'KeyH') {
+    div13.style.backgroundColor = 'yellow';
+    } else if (evento.code === 'KeyJ') {
+    div14.style.backgroundColor = 'red';
+    }
+  if (evento.code === 'KeyK') {
+    div15.style.backgroundColor = 'yellow';
+    } else if (evento.code === 'KeyL') {
+    div16.style.backgroundColor = 'red';
+    }
+  if (evento.code === 'KeyQ') {
+    div17.style.backgroundColor = 'yellow';
+    } else if (evento.code === 'KeyW') {
+    div18.style.backgroundColor = 'yellow';
+    }
+  if (evento.code === 'KeyE') {
+    div19.style.backgroundColor = 'red';
+    } else if (evento.code === 'KeyR') {
+    div20.style.backgroundColor = 'yellow';
+    }
+  if (evento.code === 'KeyT') {
+    div21.style.backgroundColor = 'red';
+    } else if (evento.code === 'KeyY') {
+    div22.style.backgroundColor = 'yellow';
+    }
+  if (evento.code === 'KeyU') {
+    div23.style.backgroundColor = 'red';
+    } else if (evento.code === 'KeyI') {
+    div24.style.backgroundColor = 'yellow';
+    }
+    })
+
+    
+    
+    
+        
+    
+div1.addEventListener('click', function() {
+  div1.style.backgroundColor = 'green';
+  });
+div2.addEventListener('click', function() {
+  div2.style.backgroundColor = 'blue';
   
-  if (key in keysToDivs) {
-    const divContent = keysToDivs[key];
-    const div = document.createElement('div');
-    div.textContent = divContent;
-    container.appendChild(div);
-  }
-});
+  });
+div3.addEventListener('click', function() {
+  div3.style.backgroundColor = 'green';
+  });
+div4.addEventListener('click', function() {
+  div4.style.backgroundColor = 'blue';
+  });
+div5.addEventListener('click', function() {
+  div5.style.backgroundColor = 'green';
+  });
+div6.addEventListener('click', function() {
+  div6.style.backgroundColor = 'green';
+  });
+div7.addEventListener('click', function() {
+  div7.style.backgroundColor = 'blue';
+  });
+div8.addEventListener('click', function() {
+  div8.style.backgroundColor = 'green';
+  });
+div9.addEventListener('click', function() {
+  div9.style.backgroundColor = 'blue';
+  });
+div10.addEventListener('click', function() {
+  div10.style.backgroundColor = 'green';
+  });
+div11.addEventListener('click', function() {
+  div11.style.backgroundColor = 'blue';
+  });
+div12.addEventListener('click', function() {
+  div12.style.backgroundColor = 'green';
+  });
+div13.addEventListener('click', function() {
+  div13.style.backgroundColor = 'green';
+  });
+div14.addEventListener('click', function() {
+  div14.style.backgroundColor = 'blue';
+  });
+div15.addEventListener('click', function() {
+  div15.style.backgroundColor = 'green';
+  });
+div16.addEventListener('click', function() {
+  div16.style.backgroundColor = 'blue';
+  });
+div17.addEventListener('click', function() {
+  div17.style.backgroundColor = 'green';
+  });
+div18.addEventListener('click', function() {
+  div18.style.backgroundColor = 'green';
+  });
+div19.addEventListener('click', function() {
+  div19.style.backgroundColor = 'blue';
+  });
+div20.addEventListener('click', function() {
+  div20.style.backgroundColor = 'green';
+  });
+div21.addEventListener('click', function() {
+  div21.style.backgroundColor = 'blue';
+  });
+div22.addEventListener('click', function() {
+  div22.style.backgroundColor = 'green';
+  });
+div23.addEventListener('click', function() {
+  div23.style.backgroundColor = 'blue';
+  });
+div24.addEventListener('click', function() {
+  div24.style.backgroundColor = 'green';
+  });
+
+  document.addEventListener('keyup',  function(evento) {
+    if (evento.code === 'KeyZ') {
+      div1.style.backgroundColor = 'white';
+      } 
+      
+      else if (evento.code === 'KeyX') {
+      div2.style.backgroundColor = 'black';
+      }
+    if (evento.code === 'KeyC') {
+      div3.style.backgroundColor = 'white';
+      } else if (evento.code === 'KeyV') {
+      div4.style.backgroundColor = 'black';
+      }
+    if (evento.code === 'KeyB') {
+      div5.style.backgroundColor = 'white';
+      } else if (evento.code === 'KeyN') {
+      div6.style.backgroundColor = 'white';
+      }
+    if (evento.code === 'KeyM') {
+      div7.style.backgroundColor = 'black';
+      } else if (evento.code === 'KeyA') {
+      div8.style.backgroundColor = 'white';
+      }
+    if (evento.code === 'KeyS') {
+      div9.style.backgroundColor = 'black';
+      } else if (evento.code === 'KeyD') {
+      div10.style.backgroundColor = 'white';
+      }
+    if (evento.code === 'KeyF') {
+      div11.style.backgroundColor = 'black';
+      } else if (evento.code === 'KeyG') {
+      div12.style.backgroundColor = 'white';
+      }
+    if (evento.code === 'KeyH') {
+      div13.style.backgroundColor = 'white';
+      } else if (evento.code === 'KeyJ') {
+      div14.style.backgroundColor = 'black';
+      }
+    if (evento.code === 'KeyK') {
+      div15.style.backgroundColor = 'white';
+      } else if (evento.code === 'KeyL') {
+      div16.style.backgroundColor = 'black';
+      }
+    if (evento.code === 'KeyQ') {
+      div17.style.backgroundColor = 'white';
+      } else if (evento.code === 'KeyW') {
+      div18.style.backgroundColor = 'white';
+      }
+    if (evento.code === 'KeyE') {
+      div19.style.backgroundColor = 'black';
+      } else if (evento.code === 'KeyR') {
+      div20.style.backgroundColor = 'white';
+      }
+    if (evento.code === 'KeyT') {
+      div21.style.backgroundColor = 'black';
+      } else if (evento.code === 'KeyY') {
+      div22.style.backgroundColor = 'white';
+      }
+    if (evento.code === 'KeyU') {
+      div23.style.backgroundColor = 'black';
+      } else if (evento.code === 'KeyI') {
+      div24.style.backgroundColor = 'white';
+      }
+      })
+  
+    
