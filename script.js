22ea1422980d250805c8a0b457a3d5e22498014b
raulenzo <raru69@hotmail.com>
2023-06-11 02:53:24 +0200

    cambios de mi rama

--- script.js
+++ script.js
@@ -28,9 +28,7 @@ let div24 = document.querySelector('#B3');
 document.addEventListener('keydown',  function(evento) {
   if (evento.code === 'KeyZ') {
     div1.style.backgroundColor = 'yellow';
-    } 
-    
-    else if (evento.code === 'KeyX') {
+    } else if (evento.code === 'KeyX') {
     div2.style.backgroundColor = 'red';
     }
   if (evento.code === 'KeyC') {
@@ -89,14 +87,11 @@ document.addEventListener('keydown',  function(evento) {
     div24.style.backgroundColor = 'yellow';
     }
     })
-
     
     
-    
-        
-    
-div1.addEventListener('click', function() {
+div1.addEventListener('click' , function() {
   div1.style.backgroundColor = 'green';
+  
   });
 div2.addEventListener('click', function() {
   div2.style.backgroundColor = 'blue';
@@ -234,4 +229,103 @@ div24.addEventListener('click', function() {
       }
       })
   
-    
+      let sound1 = new Audio('./sonidos/clave01.mp3');
+      C1.addEventListener('click', (evento)=>{
+        sound1.play();
+      })
+      let sound2 = new Audio('./sonidos/clave02.mp3');
+      C1sharp.addEventListener('click', ()=>{
+        sound2.play();
+      })   
+      let sound3 = new Audio('./sonidos/clave03.mp3');
+      D1.addEventListener('click', ()=>{
+        sound3.play();
+      })
+      let sound4 = new Audio('./sonidos/clave04.mp3');
+      D1sharp.addEventListener('click', ()=>{
+        sound4.play();
+      })   
+      let sound5 = new Audio('./sonidos/clave05.mp3');
+      E1.addEventListener('click', ()=>{
+        sound5.play();
+      })
+      let sound6 = new Audio('./sonidos/clave06.mp3');
+      F1.addEventListener('click', ()=>{
+        sound6.play();
+      })   
+      let sound7 = new Audio('./sonidos/clave07.mp3');
+      F1sharp.addEventListener('click', ()=>{
+        sound7.play();
+      })
+      let sound8 = new Audio('./sonidos/clave08.mp3');
+      G1.addEventListener('click', ()=>{
+        sound8.play();
+      })   
+      let sound9 = new Audio('./sonidos/clave09.mp3');
+      G1sharp.addEventListener('click', ()=>{
+        sound9.play();
+      })
+      let sound10 = new Audio('./sonidos/clave10.mp3');
+      A1.addEventListener('click', ()=>{
+        sound10.play();
+      })   
+      let sound11 = new Audio('./sonidos/clave11.mp3');
+      A1sharp.addEventListener('click', ()=>{
+        sound11.play();
+      })
+      let sound12 = new Audio('./sonidos/clave12.mp3');
+      B1.addEventListener('click', ()=>{
+        sound12.play();
+      })   
+      let sound13 = new Audio('./sonidos/clave13.mp3');
+      C2.addEventListener('click', ()=>{
+        sound13.play();
+      })
+      let sound14 = new Audio('./sonidos/clave14.mp3');
+      C2sharp.addEventListener('click', ()=>{
+        sound14.play();
+      }) 
+      let sound15 = new Audio('./sonidos/clave15.mp3');
+      D2.addEventListener('click', ()=>{
+        sound15.play();
+      })   
+      let sound16 = new Audio('./sonidos/clave16.mp3');
+      D2sharp.addEventListener('click', ()=>{
+        sound16.play();
+      })
+      let sound17 = new Audio('./sonidos/clave17.mp3');
+      E2.addEventListener('click', ()=>{
+        sound17.play();
+      })   
+      let sound18 = new Audio('./sonidos/clave18.mp3');
+      F2.addEventListener('click', ()=>{
+        sound18.play();
+      })
+      let sound19 = new Audio('./sonidos/clave19.mp3');
+      F2sharp.addEventListener('click', ()=>{
+        sound19.play();
+      })   
+      let sound20 = new Audio('./sonidos/clave20.mp3');
+      G2.addEventListener('click', ()=>{
+        sound20.play();
+      })
+      let sound21 = new Audio('./sonidos/clave21.mp3');
+      G2sharp.addEventListener('click', ()=>{
+        sound21.play();
+      })   
+      let sound22 = new Audio('./sonidos/clave22.mp3');
+      A3.addEventListener('click', ()=>{
+        sound22.play();
+      })
+      let sound23 = new Audio('./sonidos/clave23.mp3');
+      A3sharp.addEventListener('click', ()=>{
+        sound23.play();
+      })   
+      let sound24 = new Audio('./sonidos/clave24.mp3');
+      B3.addEventListener('click', ()=>{
+        sound24.play();
+      })
+      
+
+      
+     
