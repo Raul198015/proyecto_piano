80226a7a0fbbda521ec7035434d94397bd72d807
AkagamiNoLorea <raiseta92@gmail.com>
2023-06-13 12:48:04 +0200

    combinar paginas

--- script.js
+++ script.js
@@ -581,3 +581,15 @@ notasCheckbox.addEventListener("click", showHideNotes);
 keysCheckbox.addEventListener("click", showHideKeys);
 volumeSlider.addEventListener("input", handleVolume);
 
+var dropdown = document.querySelectorAll('nav ul li a');
+for ( var i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener('click', function(e) {
+var dropdownContent = this.nextElementSibling;
+if (dropdownContent.style.display === 'block') {
+    dropdownContent.style.display = 'none';
+} else {
+    dropdownContent.style.display = 'block';
+}
+e.preventDefault();
+    });
+}
